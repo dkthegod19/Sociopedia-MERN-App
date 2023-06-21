a5a6ab396358521dd383ce2bc614ce22d317508e
Divyam Kumar <dkthegod19@gmail.com>
2023-06-21 20:16:42 +0530

    register user function created

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import {register} from "./controllers/auth.js";
+
 
 /*Configurations */
 const __filename=fileURLToPath(import.meta.url);
@@ -35,6 +37,9 @@ const storage= multer.diskStorage({
 });
 const upload =multer({storage});
 
+
+/* Routes With Files */
+app.post("/auth/regiter",upload.single("picture"),register);
 /* Mongoose Setup */
 const port=process.env.PORT || 6000;
 mongoose.connect(process.env.MONGO_URL,{
