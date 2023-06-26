23226370fa04667028a6c2ea370fa44a33a792c4
Divyam Kumar <dkthegod19@gmail.com>
2023-06-26 21:00:54 +0530

    Navbar created

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,6 +1,10 @@
+import { Box } from "@mui/material";
+import Navbar from "../navbar";
 
 const HomePage = () =>{
-    return (<div>HomePage</div>)
+    return (<Box>
+        <Navbar></Navbar>
+    </Box>)
 };
 
 
