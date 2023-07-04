e5b60d221305cad526ab8404c9602290cbe69a85
Divyam Kumar <dkthegod19@gmail.com>
2023-07-04 19:25:37 +0530

    Post Widget Created

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -3,9 +3,9 @@ import { useSelector } from "react-redux";
 import Navbar from "../../scenes/navbar";
 import UserWidgets from "../widgets/UserWidgets";
 import MyPostWidget from "../widgets/MyPostWidget";
-import PostsWidget from "scenes/widgets/PostsWidget";
-import AdvertWidget from "scenes/widgets/AdvertWidget";
-import FriendListWidget from "scenes/widgets/FriendListWidget";
+import PostsWidget from "../widgets/PostsWidget";
+//import AdvertWidget from "../widgets/AdvertWidget";
+//import FriendListWidget from "../widgets/FriendListWidget";
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
@@ -33,9 +33,8 @@ const HomePage = () => {
         </Box>
         {isNonMobileScreens && (
           <Box flexBasis="26%">
-            <AdvertWidget />
+            
             <Box m="2rem 0" />
-            <FriendListWidget userId={_id} />
           </Box>
         )}
       </Box>
